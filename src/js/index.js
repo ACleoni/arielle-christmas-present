a82d121629c88efde6651037684b959f2c0989cb
Cleoni, Alexander L <ALEXANDER_L_CLEONI@homedepot.com>
2020-12-25 10:21:16 -0500

    card finished

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,14 @@
 import _ from "lodash";
+import "../css/style.scss";
 
-function component() {
-  const element = document.createElement("div");
-  element.innerHTML = _.join(["Hello", "Webpack"], "");
-  return element;
-}
-
-document.body.appendChild(component());
+(function () {
+  function generateSnow() {
+    const mainContainer = document.getElementById("main-container");
+    for (let i = 0; i < 200; i++) {
+      const snow = document.createElement("div");
+      snow.classList.add("snow");
+      document.body.appendChild(snow);
+    }
+  }
+  generateSnow();
+})();
